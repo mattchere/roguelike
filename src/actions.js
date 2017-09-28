173f976050491db4d60a529b4404af8f6de1598a
infinitepure <matt.j.cheah@gmail.com>
2017-09-28 23:49:02 +1000

    Create actions to enable state changing

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,37 @@
-export const changeMap = (coords, v) => {
+export const modifyMap = (coords, value) => {
   return {
-    type: 'CHANGE_MAP',
-    payload: [coords, v]
+    type: 'MODIFY_MAP',
+    payload: {
+      coords: coords,
+      value: value
+    }
+  }
+}
+
+export const switchMap = (newMap) => {
+  return {
+    type: 'SWITCH_MAP',
+    payload: {
+      newMap: newMap
+    }
+  }
+}
+
+// export const changeStat = (stat, value) => {
+//   return {
+//     type: 'CHANGE_STAT',
+//     payload: {
+//       stat: stat,
+//       value: value
+//     }
+//   }
+// }
+
+export const moveMap = (direction) => {
+  return {
+    type: 'MOVE_MAP',
+    payload: {
+      direction: direction
+    }
   }
 }
